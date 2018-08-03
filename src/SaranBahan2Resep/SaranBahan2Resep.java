f5562edac04ef68d22cb5738500a59db3f11791e
Naufal K D <Naufal K D@DESKTOP-FANJPB3>
2018-08-03 12:55:50 +0700

    Initial Commit

---
+++ src/SaranBahan2Resep/SaranBahan2Resep.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SaranBahan2Resep;
+
+/**
+ *
+ * @author Naufal K D
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Naufal Kharisma Dewangga / XR5 / 31";
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+    }
+    
+}
