cc3554b0efbd630e05acf128dd92ce45320e4b1e
Naufal K D <Naufal K D@DESKTOP-FANJPB3>
2018-08-03 11:20:07 +0700

    Coba IF

--- src/CobaPercabangan/CobaPercabangan.java
+++ src/CobaPercabangan/CobaPercabangan.java
@@ -5,6 +5,8 @@
  */
 package CobaPercabangan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Naufal K D
@@ -14,6 +16,21 @@ public class CobaPercabangan {
         String identitas = "Naufal Kharisma Dewangga / XRPL5 / 31";
         System.out.println("identitas : "+ identitas);
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. l");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        
+        System.out.println("\nIF.. 3");
+        if(a > 5)
+            System.out.println("isi variable a : "+ a);
+            System.out.println("nilai a lebih dari 5");
     }
     
 }
