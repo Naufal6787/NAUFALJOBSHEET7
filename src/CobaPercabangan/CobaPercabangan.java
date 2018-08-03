932e76a866e5b82e09585a574bf6bf44b216a394
Naufal K D <Naufal K D@DESKTOP-FANJPB3>
2018-08-03 11:07:22 +0700

    Initial Commit

---
+++ src/CobaPercabangan/CobaPercabangan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CobaPercabangan;
+
+/**
+ *
+ * @author Naufal K D
+ */
+public class CobaPercabangan {
+    public static void main(String[] args) {
+        String identitas = "Naufal Kharisma Dewangga / XRPL5 / 31";
+        System.out.println("identitas : "+ identitas);
+        
+    }
+    
+}
